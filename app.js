2fcd0c9311f76ce3e4a9fd3c1d7e417ac228a52b
Deby2404 <983319814+Deby2404@users.noreply.github.com>
2022-03-29 14:49:06 +0900

    rock scissors paper

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-let alertString;
-alertString = addString("webcamp");
+// 入力するプロンプト
+let user_hand = prompt('choose rock, scissors, or paper');
+// じゃんけんの手をランダムに作成する関数
+let js_hand = getJShand();
+// ユーザーとJSを比べる関数を呼び出して、judgeに代入
+let  judge = winLose(user_hand, js_hand);
+// 結果表示
+alert("you choose " + user_hand + "and JS choose " + js_hand + " the winner is " + judge);
 
-alert(alertString)
+// ランダムでじゃんけんを作成する関数
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
+  let hand_name;
 
-function addString(strA) {
-  let addStr = "hello!" + strA;
-  return addStr;
+if (js_hand_num == 0) {
+  hand_name = "rock";
+} else if(js_hand_num == 1){
+  hand_name = "scissors";
+}else if(js_hand_num == 2){
+  hand_name = "paper";
 }
+
+return hand_name;
+}
+
+// ユーザーとJSの手を比べる関数
+function winLose(user, js) {
+  let winLoseStr;
+// ユーザーがグーを出した場合
+  if (user == "rock"){
+    if(js == "rock"){
+      winLoseStr = "again!";
+    }else if(js == "scissors"){
+      winLoseStr = "win!";
+    }else if(js == "paper"){
+      winLoseStr = "loose!";
+    }
+    // ユーザーがチョキを出した場合
+  } else if(user == "scissors"){
+    if (js == "rock"){
+      winLoseStr = "loose!";
+    }else if(js == "scissors"){
+      winLoseStr = "again!";
+    }else if(js == "paper"){
+      winLoseStr = "win!";
+    }
+    // ユーザーがパーを出した場合
+  }else if(user == "paper"){
+    if (js == "rock"){
+      winLoseStr = "win!";
+    }else if(js == "scissors"){
+      winLoseStr = "loose!";
+    }else if(js == "paper"){
+      winLoseStr = "again!";
+    }
+  }
+  return winLoseStr;
+  }
